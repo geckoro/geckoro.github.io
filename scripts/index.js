c2ae79db8c690d6421643ce7c349d119290514da
George <gdusa@deltapi.nl>
2023-09-25 22:08:14 +0300

    Beautifed index, organized code better

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+function scale(number, inMin, inMax, outMin, outMax) {
+  return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+$("#button-clouds").on("click", function () {
+  window.location.href = "pages/clouds.html";
+});
+
+$(function () {
+  $(document).mousemove(function (event) {
+    $('.circle').css('width', '1000px');
+    $('.circle').css('height', '1000px');
+    let dimension = $(".circle").width();
+    $('.circle').css('left', event.pageX - dimension / 2);
+    $('.circle').css('top', event.pageY - dimension / 2);
+    let r = scale(event.pageX, 0, $(window).width(), 0, 255);
+    let g = scale(event.pageY, 0, $(window).height(), 255, 0);
+    let b = scale(event.pageX, 0, $(window).width(), 255, 0);
+    $('.circle').css('background-image', 'radial-gradient(50% 30%, rgba(' + r + ',' + g + ',' + b + ', 0.1), rgba(0,0,0,0)');
+  });
+});
